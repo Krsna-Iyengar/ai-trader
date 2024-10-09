6b82ad3649ce513d1afd37d5486a7bd08f13a484
krsna <krsna.iyengar@gmail.com>
2024-10-09 18:49:39 +0530

    unnecessary firebase ref removed

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,22 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-import { initializeApp } from 'firebase/app';
 import axios from 'axios';  // Import Axios for HTTP requests
 import './SignUp.css';
 
-const firebaseConfig = {
   
-    
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
-  
-  const app = initializeApp(firebaseConfig);
+
   const auth = getAuth(app);
 
 function SignUp({ darkMode }) {
